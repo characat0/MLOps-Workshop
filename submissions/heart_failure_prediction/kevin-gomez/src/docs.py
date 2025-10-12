4636c8591f735e950a407c08589ae8a969a15bce
kevin <kevin.gomez.villanueva.uni@outlook.com>
2025-10-12 13:29:31 -0500

    heart failure prediction

---
+++ submissions/heart_failure_prediction/kevin-gomez/src/docs.py
@@ -0,0 +1,20 @@
+import cloudpickle
+import os
+
+
+current_file = __file__
+
+
+data_folder = os.path.join(os.path.dirname(current_file), "..", "data")
+data_file = os.path.join(data_folder, "heart.csv")
+model_path = os.path.join(data_folder, "model", "model.pkl")
+
+model = cloudpickle.load(open(model_path, "rb"))
+
+names = model.feature_names_in_.tolist()
+
+
+print("Feature names used in the model:")
+for name in names:
+    print(f"- {name}")
+
